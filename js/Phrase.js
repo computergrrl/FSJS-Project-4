1cc1da7be5b6f5c3ecf374796a9eb3eb151a055f
Amber Stevens <anani.mowse@gmail.com>
2019-09-28 13:49:44 -0700

    Phrase displays but wtf is this "undefined" before it?

--- js/Phrase.js
+++ js/Phrase.js
@@ -10,10 +10,29 @@
 
     }
 
+    //method to split string into an array of characters
+    getLetters() {
+
+      return this.phrase.split("");
+
+    }
 
     addPhraseToDisplay() {
 
+      let letters; //variable to hold html string
+      this.getLetters().forEach( letter => {//loop through characters
 
+        if(letter == " ") { // if character is a space add space class
+          letters += '<li class="space">' +letter + '</li>';
+
+        } else {  //add hide letter class to the letter
+            letters += '<li class="hide letter">' +letter + '</li>';
+
+        }
+
+
+      });
+      return letters;
     }
 
 
--- js/app.js
+++ js/app.js
@@ -2,21 +2,19 @@
  * Project 4 - OOP Game App
  * app.js */
 
-
+ //declare some global variables
  const startButton = document.getElementById("btn__reset");
- //const overlay = document.getElementById("overlay");
-
+ const phraseDisplay = document.querySelector("#phrase ul");
  let game;
  let phrase;
+
+
  startButton.addEventListener("click" , () => {
 
-   game = new Game(0, thephrases);
-   game.startGame();
-   phrase = new Phrase(game.activePhrase)
-   // console.log(game.getRandomPhrase());
-   // console.log(phrase);
+   game = new Game(0, thephrases); //instantiate new Game object
+   game.startGame(); //call startGame method
+   phrase = new Phrase(game.activePhrase); //instantiate Phrase object
 
-  // console.log(game);
-   console.log(phrase)
 
+    phraseDisplay.innerHTML = phrase.addPhraseToDisplay();
 });
